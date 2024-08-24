f032d6d4128c3f3fd38edb2c9f88fdf0bb8c9ede
felipecintraferraz <137323389+felipecintraferraz@users.noreply.github.com>
2024-08-24 00:00:18 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,13 +11,10 @@ echo "Starting script to generate Allure Report ..."
 echo "Getting results from $RESULTS_DIRECTORY"
 echo "Final report will be stored at $REPORT_DIRECTORY"
 
-cp -r allure-report/history $RESULTS_DIRECTORY
-
 if [ ! -d "$REPORT_DIRECTORY" ]; then
   echo "creating report directory"
   mkdir -p $REPORT_DIRECTORY
-  echo "listing report directory"
-  ls -l $REPORT_DIRECTORY
+  echo "directory $REPORT_DIRECTORY created"
 fi
 
 if [ -d "$RESULTS_HISTORY" ]; then
